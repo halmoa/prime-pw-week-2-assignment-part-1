d528ec6cd60c28b7ec27e19377a0cdc0ce9e4ca3
Halima Moalim <halimaamoalim@gmail.com>
2022-06-14 11:37:55 -0500

    basics

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -9,13 +9,13 @@ let fullName = firstName  + lastName;
 // 4 - Console log the value of `fullName`
 console.log(fullName);
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
-let luckyNumber = 9;
+let luckyNumber = 8;
 // 6 - Console log this sentence, adding in the variables you created above: 
 // 'My name is (full name), and I think (lucky number) is a winner!'.
 // Refer back to the videos if you need help with this one.
 console.log('My name is', fullName, 'and I think', luckyNumber, 'is a winner!');
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
-let adventurous = True;
+let adventurous = true;
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
 let food = 'Pesto Pasta';
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
@@ -28,16 +28,37 @@ pets = 2;
 const allowedPets = 4;
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
 // if it's not true, console log "How about we stay home?"
-
+if(adventurous){
+console.log('Adventures are great!')
+}
+else {
+    console.log('How about we stay home?');
+}
 
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
+if( luckyNumber === 2 && adventurous){
+console.log('Roll the dice!');
+}
+else{
+    console.log('No dice roll');
+}
+
 
 // 15 - Write a conditional that console logs "I can have more pets!" 
 //if the value of `pets` is less than the value of `allowedPets`,
 // console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
 // and console logs "Oh no, I have too many pets!"
 // if the value of `pets` is greater than the value of `allowedPets`.
+if(pets < allowedPets){
+console.log('I can have enough pets');
+}
+else if(pets === allowedPets){
+console.log('I have enough pets');
+}
+else if(pets > allowedPets){
+console.log('Oh no, I have too many pets');
+}
 
 // STRETCH GOALS:
 
